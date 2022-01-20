b500978008f7f8a92f27da25d1e635e4a731ae86
Alex Harker <ajharker@gmail.com>
2022-01-20 12:40:03 +0000

    Update config / resource

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
-#define PLUG_NAME "HISSTools Granular"
+#define PLUG_NAME "Octet Violins"
 #define PLUG_MFR "Alex Harker"
 #define PLUG_VERSION_HEX 0x00010000
 #define PLUG_VERSION_STR "1.0.0"
-#define PLUG_UNIQUE_ID 'HTGn'
+#define PLUG_UNIQUE_ID 'AHOV'
 #define PLUG_MFR_ID 'AHAR'
 #define PLUG_URL_STR "www.alexanderjharker.co.uk"
 #define PLUG_EMAIL_STR "A.Harker@hud.ac.uk"
